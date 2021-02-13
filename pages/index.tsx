8036cb5ffb6ca7e341a9308a433bb1aba35153b9
Mike Rudenko <m.rudenko@app-smart.ru>
2021-02-13 14:13:15 +0300

    Add terms and contacts pages

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,53 +1,59 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.scss'
+import Link from 'next/link'
+
 
 export default function Home() {
-  const linkToApp = "https://apps.apple.com/app/id1498416482";
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>ABC Transport</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    const linkToApp = "https://apps.apple.com/app/id1498416482";
+    return (
+        <div className={styles.container}>
+            <Head>
+                <title>ABC Transport</title>
+                <link rel="icon" href="/favicon.ico"/>
+            </Head>
 
-      <main className={styles.main}>
+            <main className={styles.main}>
 
-        <a href={linkToApp}>
-          <img className={styles.appLogo}
-             src="/ios-marketing.jpeg" alt="application logo"/>
-        </a>
+                <a href={linkToApp}>
+                    <img className={styles.appLogo}
+                         src="/ios-marketing.jpeg" alt="application logo"/>
+                </a>
 
-        <h1 className={styles.title}>
-          <a href={linkToApp}> ABC Transport </a>
-        </h1>
+                <h1 className={styles.title}>
+                    <a href={linkToApp}> ABC Transport </a>
+                </h1>
 
-          <p className={styles.description}>
-            One of the best way to learn{' '}
-            <code className={styles.code}>English</code>
-          </p>
+                <p className={styles.description}>
+                    One of the best way to learn{' '}
+                    <code className={styles.code}>English</code>
+                </p>
 
-        <a href={linkToApp}>
-          <img className={styles.appstore}
-               src="/download-appstore-black.svg" alt="application logo"/>
-        </a>
+                <a href={linkToApp}>
+                    <img className={styles.appstore}
+                         src="/download-appstore-black.svg" alt="ABC Transport logo"/>
+                </a>
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Terms of use &rarr;</h3>
-            <p>Our information about content, copyrights and Third-Party Links</p>
-          </a>
+                <div className={styles.grid}>
+                    <Link href="/terms">
+                        <a className={styles.card}>
+                            <h3>Terms of use &rarr;</h3>
+                            <p>Our information about content, copyrights and Third-Party Links</p>
+                        </a>
+                    </Link>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Contact form &rarr;</h3>
-            <p>You can send message for communication with developer</p>
-          </a>
+                    <Link href="/contacts">
+                        <a className={styles.card}>
+                            <h3>Contact form &rarr;</h3>
+                            <p>You can send message for communication with developer</p>
+                        </a>
+                    </Link>
 
+                </div>
+            </main>
+
+            <footer className={styles.footer}>
+                All rights reserved © Elizaveta Rudenko
+            </footer>
         </div>
-      </main>
-
-      <footer className={styles.footer}>
-        All rights reserved © Elizaveta Rudenko
-      </footer>
-    </div>
-  )
+    )
 }
